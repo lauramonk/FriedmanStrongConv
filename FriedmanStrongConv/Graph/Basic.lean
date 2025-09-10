58677a7538c6041e870236ced5cb52eb64d473a0
Laura Monk <laura.monk@bristol.ac.uk>
2025-09-10 12:06:17 +0100

    Removed Basic notions after mathlib import

--- FriedmanStrongConv/Graph/Basic.lean
+++ FriedmanStrongConv/Graph/Basic.lean
@@ -2,23 +2,4 @@ import Mathlib.Combinatorics.Graph.Basic
 
 /- Additions to Graph.Basic -/
 
-namespace Graph
-
-variable {α β : Type*} {G : Graph α β} {x : α}
-
-/-- `G.incSet x` is the set of edges incident to `x` in `G`. -/
-def incSet (x : α) : Set β := {e | G.Inc e x}
-
-@[simp]
-theorem mem_incSet (x : α) (e : β) : e ∈ G.incSet x ↔ G.Inc e x :=
-  Iff.rfl
-
-/-- `G.loopSet x` is the set of loops at `x` in `G`. -/
-def loopSet (x : α) : Set β := {e | G.IsLoopAt e x}
-
-@[simp]
-theorem mem_loopSet (x : α) (e : β) : e ∈ G.loopSet x ↔ G.IsLoopAt e x :=
-  Iff.rfl
-
-/-- The loopSet is included in the incSet-/
-theorem loopSet_subset (x : α): G.loopSet x ⊆ G.incSet x := fun _ he ↦ ⟨x, he⟩
+/- Has been merged onto Mathlib -/
