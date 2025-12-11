467f8876e6cda828ba4875002688ff5b221f7f24
Laura Monk <laura.monk@bristol.ac.uk>
2025-12-11 14:56:27 +0000

    Update

--- FriedmanStrongConv.lean
+++ FriedmanStrongConv.lean
@@ -1,2 +1,7 @@
 import FriedmanStrongConv.Example
+import FriedmanStrongConv.Graph.Basic
+import FriedmanStrongConv.Graph.Connectivity
 import FriedmanStrongConv.Graph.Finite
+import FriedmanStrongConv.Graph.Walks.Basic
+import FriedmanStrongConv.Graph.Walks.Operations
+import FriedmanStrongConv.Graph.Walks.«Basic copy»
