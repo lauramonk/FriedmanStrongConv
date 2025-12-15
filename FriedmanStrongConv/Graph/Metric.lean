3de1595f376673d65dbbeff1d34643c29743f3af
Laura Monk <laura.monk@bristol.ac.uk>
2025-12-15 14:06:23 +0000

    Regular treee as set of reduced words

--- FriedmanStrongConv/Graph/Metric.lean
+++ FriedmanStrongConv/Graph/Metric.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Laura Monk. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kyle Miller, Vincent Beffara, Rida Hamadani
+Authors: Laura Monk
 -/
 
 import FriedmanStrongConv.Graph.Connectivity
