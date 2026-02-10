efd138ae1bebc2bdfbc972ab7749257d40d3c1d8
Laura Monk <laura.monk@bristol.ac.uk>
2026-02-10 15:19:20 +0000

    New version Darts

--- FriedmanStrongConv/Graph/Dart.lean
+++ FriedmanStrongConv/Graph/Dart.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Laura Monk. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Laura Monk
+Authors: Laura Monk and Freddie Nash
 -/
 
 import Mathlib.Combinatorics.Graph.Basic
@@ -23,17 +23,41 @@ variable {G : Graph α β}
 
 /-- A dart is describes a possible direction for a walk starting at one point.
 In order to count walks correctly, we adopt the convention that each loop
-can be taken in two distinct directions, encoded in orienOfEq. -/
-@[ext]
-structure Dart where
-  fst : α
-  snd : α
-  edge : β
-  isLink : G.IsLink edge fst snd
-  orienOfEq : fst = snd -> Bool
-  deriving DecidableEq
+can be taken in two distinct directions, forwards or backwards. -/
+inductive Dart
+  | Dir : (x y : α) -> (e : β) -> (ne : x ≠ y) -> (h : G.IsLink e x y) -> Dart
+  | Fwd : (x : α) -> (e : β) -> (h : G.IsLoopAt e x) -> Dart
+  | Bck : (x : α) -> (e : β) -> (h : G.IsLoopAt e x) -> Dart
 
 namespace Dart
+
+/-- The map `fst` extracts the starting point of a dart. -/
+def fst (d : G.Dart) : α :=
+  match d with
+  | .Dir x _ _ _ _ => x
+  | .Fwd x _ _ => x
+  | .Bck x _ _ => x
+
+/-- The map `snd` extracts the end point of a dart. -/
+def snd (d : G.Dart) : α :=
+  match d with
+  | .Dir _ y _ _ _ => y
+  | .Fwd x _ _ => x
+  | .Bck x _ _ => x
+
+/-- The map `edge` extracts the edge corresponding to a dart. -/
+def edge (d : G.Dart) : β :=
+  match d with
+  | .Dir _ _ e _ _ => e
+  | .Fwd _ e _ => e
+  | .Bck _ e _ => e
+
+/-- The map `isLink` extracts the link relation from the definition of the dart. -/
+def isLink (d : G.Dart) : G.IsLink d.edge d.fst d.snd :=
+  match d with
+  | .Dir _ _ _ _ h => h
+  | .Fwd _ _ h => h
+  | .Bck _ _ h => h
 
 lemma fst_mem (d : G.Dart) : d.fst ∈ V(G) := d.isLink.left_mem
 
@@ -42,33 +66,38 @@ lemma snd_mem (d : G.Dart) : d.snd ∈ V(G) := d.isLink.right_mem
 lemma edge_mem (d : G.Dart) : d.edge ∈ E(G) := d.isLink.edge_mem
 
 /-- The reversing operation on darts, which reverses its orientation. -/
-def reverse (d : G.Dart) : G.Dart :=
-  Dart.mk d.snd d.fst d.edge d.isLink.symm (fun h => (d.orienOfEq h.symm).not)
+noncomputable def reverse (d : G.Dart) : G.Dart :=
+    match d with
+  | .Dir x y e ne h => Dir y x e ne.symm h.symm
+  | .Fwd x e h => Bck x e h
+  | .Bck x e h => Fwd x e h
 
 /-- The start point of the reverse dart is its end point. -/
-lemma fst_of_reverse (d : G.Dart) : d.reverse.fst = d.snd := rfl
+lemma fst_of_reverse (d : G.Dart) : d.reverse.fst = d.snd := by cases d <;> trivial
 
 /-- The end point of the reverse dart is its start point. -/
-lemma snd_of_reverse (d : G.Dart) : d.reverse.snd = d.fst := rfl
+lemma snd_of_reverse (d : G.Dart) : d.reverse.snd = d.fst := by cases d <;> trivial
 
 /-- The edge is unchanged upon reversing the dart. -/
-lemma edge_of_reverse (d : G.Dart) : d.reverse.edge = d.edge := rfl
+lemma edge_of_reverse (d : G.Dart) : d.reverse.edge = d.edge := by cases d <;> trivial
 
 /-- The reverse of a dart is distinct from the dart. -/
 lemma reverse_neq_self (d : G.Dart) : d.reverse ≠ d := by
-  by_cases h : d.fst = d.snd
-  · sorry
-  · sorry
+  cases d <;> intro hn <;> cases hn; contradiction
 
 /-- The reverse of the reverse of a dart is the dart itself. -/
-lemma reverse_of_reverse (d : G.Dart) : d.reverse.reverse = d := sorry
+lemma reverse_of_reverse (d : G.Dart) : d.reverse.reverse = d := by cases d <;> rfl
 
 end Dart
 
 /-- The dartset of a vertex is the set of darts starting at this vertex. -/
 def dartSet (x : α) : Set G.Dart := {d | d.fst = x}
 
-lemma dart_of_edge (e : E(G)) : ∃ d : G.Dart, d.edge = e := sorry
+def toDart [DecidableEq α] {x y : α} {e : β} (h : G.IsLink e x y) : G.Dart := by
+  by_cases eq : x = y
+  · have hx : G.IsLink e x x := by rw [eq.symm] at h; exact h
+    exact Dart.Fwd x e hx
+  . exact Dart.Dir x y e eq h
 
 /-- Two darts have the same edge iff they are equal or reverse of one another. -/
 lemma edge_dart_eq_iff {d₁ d₂ : G.Dart} (h : d₁.edge = d₂.edge) : d₁ = d₂ ∨ d₁ = d₂.reverse := sorry
