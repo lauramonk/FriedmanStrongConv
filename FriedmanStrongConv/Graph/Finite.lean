1d5291822a90eb805410d62dff240797860b4bbd
Laura Monk <laura.monk@bristol.ac.uk>
2026-02-20 16:09:01 +0000

    Update references in blueprint

--- FriedmanStrongConv/Graph/Finite.lean
+++ FriedmanStrongConv/Graph/Finite.lean
@@ -1,4 +1,7 @@
 import Mathlib.Combinatorics.Graph.Basic
+import FriedmanStrongConv.Graph.Dart
+import Mathlib.Data.ENat.Lattice
+
 
 /-!
 # Definitions for finite and locally finite graphs
@@ -10,3 +13,18 @@ graph, as well as the notion of degree, and elementary properties.
 ## Main definitions
 [TODO]
 -/
+
+universe u v
+variable {α : Type u} {β : Type v} {x y z u v w : α} {e f : β}
+
+namespace Graph
+
+section FiniteAt
+
+/-!
+## Finiteness at a vertex and notion of degree at this vertex
+
+This section contains the notion of finiteness and degree at the vertex `x`.
+-/
+
+def edegree (x : α) : ℕ∞ := set.ncard dartSet x
